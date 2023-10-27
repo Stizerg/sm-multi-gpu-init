b1425bdc57b257850100160b4faf60e8c7292a09
Stizerg <105415949+Stizerg@users.noreply.github.com>
2023-10-28 10:45:59 +1100

    Update sm-multi-gpu-init.sh
    Initialization will continue from where it was interrupted

--- sm-multi-gpu-init.sh
+++ sm-multi-gpu-init.sh
@@ -5,6 +5,7 @@
 # When the provider finishes initializing the file, it is given the next one.
 # In this way, all providers will be used until there are no files left in the initialization queue.
 # Even if the providers are different in power, this will not lead to downtime for the more powerful ones.
+# Initialization will continue from where it was interrupted
 
 # Get postcli: https://github.com/spacemeshos/post/releases
 # RTM https://github.com/spacemeshos/post/blob/develop/cmd/postcli/README.md
@@ -17,23 +18,66 @@
 # edit this section
 providers=(0 1)  # in this case GPU0 and GPU1 are used (also you can use CPU, its number is 4294967295)
 atx="8B3391AB8C7E3B43D5E941EC81C6A78E91054E11ADA48BDEC5F2CE076F9CA4DA"  # Use latest Highest ATX (Hex)
-nodeId="102023cabf7a274ec817d556653bc98b441ba6523b3e56cdcb137ae5a49ab3c"  # Your public nodeId (smehserId)
+nodeId="5421252cabf7a274ec817d524723bc44b441ba6592b3e12cdcb137ae5a49ab3c"  # Your public nodeId (smehserId)
 fileSize=$((2 * 1024 * 1024 * 1024))  # 2 GiB  (For larger volumes, for convenience, you can increase to 4,8,16+ GiB)
 startFromFile=0
 numUnits=4  # 64 GiB each (mininum 4)
-dataDir="/mnt/spacemesh/post" 
+dataDir="./post"
 # end edit section
 
 filesTotal=$(($numUnits * 64 * 1024 * 1024 * 1024 / $fileSize))
-echo "filesTotal $filesTotal"
+echo "Total files $filesTotal"
 declare -A processes
+declare -a filesArray
 fileCounter=$startFromFile
 
-while (( fileCounter < filesTotal )); do
+# Creating an array of existing files
+while IFS= read -r line; do
+    dirName=$(dirname "$line")
+    dirNum=${dirName##*/}
+    
+    fileName=$(basename "$line")
+    fileNum=${fileName#*_}
+    fileNum=${fileNum%.*}
+    
+    filesArray+=("$dirNum:$fileNum")
+done < <(find . -type f -name 'postdata_*.bin')
+
+IFS=$'\n' filesArray=($(sort -t ":" -k2n <<<"${filesArray[*]}"))
+unset IFS
+
+# Checking of already created files, continue if unfinished
+amt=${#filesArray[@]}
+while ((fileCounter < amt)); do
 	for p in "${providers[@]}"; do
+		postfile=${filesArray[fileCounter]}
+		oldIFS=$IFS
+	    IFS=":"
+	    read -ra pFile <<< "$postfile"
+	    IFS=$oldIFS
+	    dirNum=${pFile[0]}
+
+	    if [ -z "${processes[$p]}" ] || ! kill -0 ${processes[$p]} 2> /dev/null; then
+		    echo "Checking file postdata_$fileCounter.bin in $dataDir/$dirNum"
+		    echo "-------------------------------------------------"
+		    ./postcli -provider $p -commitmentAtxId $atx -id $nodeId -labelsPerUnit 4294967296 -maxFileSize $fileSize -numUnits $numUnits -datadir $dataDir/$dirNum -fromFile $fileCounter -toFile $fileCounter & processes[$p]=$!
+		    ((fileCounter++))
+		    if [[ "$fileCounter" -eq "$amt" ]] ; then # additional check in case if the number of providers is different
+		        break
+		    fi
+	    fi
+    done
+    sleep 1
+done
+
+# All previously created files have been done, continue normal procedure
+echo "We will continue a normal initialization starting from file $fileCounter"
+echo "-----------------------------------------------------------------"
+while (( fileCounter < filesTotal )); do
+    for p in "${providers[@]}"; do
             if [ -z "${processes[$p]}" ] || ! kill -0 ${processes[$p]} 2> /dev/null; then
                 echo " provider $p starts file $fileCounter out of $filesTotal"
-                echo "----------------------------------------------"
+                echo "-------------------------------------------------"
                 ./postcli -provider $p -commitmentAtxId $atx -id $nodeId -labelsPerUnit 4294967296 -maxFileSize $fileSize -numUnits $numUnits -datadir $dataDir/$p -fromFile $fileCounter -toFile $fileCounter & processes[$p]=$!
                 ((fileCounter++))
             fi
@@ -41,5 +85,5 @@ while (( fileCounter < filesTotal )); do
     sleep 1
 done
 
-echo "done"
+echo "Done!"
 read -p "Press enter to continue"

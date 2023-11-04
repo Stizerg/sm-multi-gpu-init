ac89c6939e6e8e320b36384ebebec08aa32e8835
Stizerg <105415949+Stizerg@users.noreply.github.com>
2023-11-04 11:59:50 +1100

    Update
    Doesn't use subfolder anymore
    Checking for missing files
    The speed of existing files checking increased
    postcli priority is set to low

--- sm-multi-gpu-init.sh
+++ sm-multi-gpu-init.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Version 1.0.4 
+# Version 1.1.0
 # The program initializes files sequentially.
 # Each file is initialized by one provider.
 # When the provider finishes initializing the file, it is given the next one.
@@ -10,7 +10,6 @@
 
 # Get postcli: https://github.com/spacemeshos/post/releases
 # RTM https://github.com/spacemeshos/post/blob/develop/cmd/postcli/README.md
-# After completing the initialization of the subsets, you will need to combine the files into same direcory and merge postmeta_data.json as described in the README.md
 #
 # Based on Samovar's (PlainLazy) Powershell script
 # https://github.com/PlainLazy/crypto
@@ -19,7 +18,7 @@
 # edit this section
 providers=(0 1)  # in this case GPU0 and GPU1 are used (also you can use CPU, its number is 4294967295)
 atx="DDDC920DD577749ECF0F2CC8E96D38C3F792C71350780090AD6F63164C519BE6"  # Use latest Highest ATX (Hex)
-nodeId="3110232cabf7a274ec817d524723bc44b441ba6592b3e12cdcb137ae5a49ab3c"  # Your public nodeId (smehserId)
+nodeId="0411201366addcec555ecca5115839706582f896d85521456814e301066886c8"  # Your public nodeId (smehserId)
 fileSize=$((2 * 1024 * 1024 * 1024))  # 2 GiB  (For larger volumes, for convenience, you can increase to 4,8,16+ GiB)
 startFromFile=0
 numUnits=4  # 64 GiB each (mininum 4)
@@ -31,63 +30,90 @@ echo "Total files $filesTotal"
 declare -A processes
 declare -a filesArray
 fileCounter=$startFromFile
+mainLoop=true
+expectedFileNum=0
+providerCounter=0
+providersLength=${#providers[@]}
 
-# Creating an array of existing files
+# Creating an array of existing or missing files
 while IFS= read -r line; do
     dirName=$(dirname "$line")
-    dirNum=${dirName##*/}
+    #dirNum=${dirName##*/}
     
     fileName=$(basename "$line")
     fileNum=${fileName#*_}
     fileNum=${fileNum%.*}
     
-    filesArray+=("$dirNum:$fileNum")
+    filesArray+=("$dirName:$fileNum")
 done < <(find "$dataDir" -type f -name 'postdata_*.bin')
 
 IFS=$'\n' filesArray=($(sort -t ":" -k2n <<<"${filesArray[*]}"))
 unset IFS
 
-# Checking of already created files, continue if unfinished
-amt=${#filesArray[@]}
-while ((fileCounter < amt)); do
-	for p in "${providers[@]}"; do
-		postfile=${filesArray[fileCounter]}
-		oldIFS=$IFS
-	    IFS=":"
-	    read -ra pFile <<< "$postfile"
-	    IFS=$oldIFS
-	    dirNum=${pFile[0]}
+for i in "${!filesArray[@]}"; do
+    currentFileNum=${filesArray[$i]#*:}
 
-	    if [ -z "${processes[$p]}" ] || ! kill -0 ${processes[$p]} 2> /dev/null; then
-		    echo "Checking file postdata_$fileCounter.bin in $dataDir/$dirNum"
-		    echo "-------------------------------------------------"
-		    nice -n 19 ./postcli -provider $p -commitmentAtxId $atx -id $nodeId -labelsPerUnit 4294967296 -maxFileSize $fileSize -numUnits $numUnits -datadir $dataDir/$dirNum -fromFile $fileCounter -toFile $fileCounter & processes[$p]=$!
-		    ((fileCounter++))
-		    if [[ "$fileCounter" -eq "$amt" ]] ; then # additional check in case if the number of providers is different
-		        break
-		    fi
-	    fi
+    while (( currentFileNum != expectedFileNum )); do
+        currentProvider=${providers[$providerCounter]}
+        filesArray+=("$dataDir/$currentProvider:$expectedFileNum")
+        ((providerCounter=(providerCounter+1)%providersLength))
+        ((expectedFileNum++))
     done
-    sleep 1
+
+    ((expectedFileNum++))
 done
 
-# All previously created files have been done, continue normal procedure
-if [[ $fileCounter -gt 0 ]]; then
-echo "We will continue a normal initialization starting from file $fileCounter"
-echo "-----------------------------------------------------------------"
+IFS=$'\n' filesArray=($(sort -t ":" -k2n <<<"${filesArray[*]}"))
+unset IFS
+
+# Checking of already created files, continue initialization if unfinished
+amt=${#filesArray[@]}
+if [[ $amt -gt 0 ]]; then
+	while ((fileCounter < amt)); do
+		for p in "${providers[@]}"; do
+			postFile=${filesArray[fileCounter]}
+			oldIFS=$IFS
+	    	IFS=":"
+	    	read -ra pFile <<< "$postFile"
+	    	IFS=$oldIFS
+	    	dirName=${postFile%%:*}
+
+	    	if [ -z "${processes[$p]}" ] || ! kill -0 ${processes[$p]} 2> /dev/null; then
+		    	echo "Checking existing file: $dirName/postdata_$fileCounter.bin"
+		    	echo "-----------------------------------------------------------------"
+		    	nice -n 19 ./postcli -provider $p -commitmentAtxId $atx -id $nodeId -labelsPerUnit 4294967296 -maxFileSize $fileSize -numUnits $numUnits -datadir $dirName -fromFile $fileCounter -toFile $fileCounter & processes[$p]=$!
+		    	((fileCounter++))
+		    	if [[ "$fileCounter" -eq "$filesTotal" ]]; then
+		        	mainLoop=false
+		        	break 2
+		    	fi
+		    	if [[ "$fileCounter" -eq "$amt" ]] ; then # additional check in case if the number of providers changed
+		        	break
+		    	fi
+	    	fi
+    	done
+    	sleep 0.2
+	done
 fi
 
-while (( fileCounter < filesTotal )); do
-    for p in "${providers[@]}"; do
+# All previously created files have been done, we continue with the normal procedure
+if $mainLoop; then
+	while (( fileCounter < filesTotal )); do
+    	for p in "${providers[@]}"; do
             if [ -z "${processes[$p]}" ] || ! kill -0 ${processes[$p]} 2> /dev/null; then
-                echo " provider $p starts file $fileCounter out of $filesTotal"
-                echo "-------------------------------------------------"
-                nice -n 19 ./postcli -provider $p -commitmentAtxId $atx -id $nodeId -labelsPerUnit 4294967296 -maxFileSize $fileSize -numUnits $numUnits -datadir $dataDir/$p -fromFile $fileCounter -toFile $fileCounter & processes[$p]=$!
+                echo " Provider $p starts processing a new file: $dataDir/postdata_$fileCounter.bin (Total files:$filesTotal)"
+                echo "-----------------------------------------------------------------"
+                nice -n 19 ./postcli -provider $p -commitmentAtxId $atx -id $nodeId -labelsPerUnit 4294967296 -maxFileSize $fileSize -numUnits $numUnits -datadir $dataDir -fromFile $fileCounter -toFile $fileCounter & processes[$p]=$!
                 ((fileCounter++))
             fi
-    done
-    sleep 1
-done
+    	done
+    	sleep 1
+	done
+fi
 
-echo "Done!"
+echo "Almost there"
+echo "Waiting for background processes to finish"
+echo "------------------------------------------"
+wait
+echo "All done!"
 read -p "Press enter to continue"
